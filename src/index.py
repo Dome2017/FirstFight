aa614f53e965cc752b3a837fa3c4f8303565966f
Dome2017 <dominik.monkos@gmail.com>
2024-09-21 21:16:44 +0100

    otwarcie drugiego okna przeniesienie imienia na drugie okno

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from battle_interface import otworz_drugie_okno
 
 
 def shown_chosen_name():
@@ -8,6 +9,8 @@ def shown_chosen_name():
         messagebox.showerror("Error", "Name must be chosen")
     else:
         input_character_name.config(text=f"You will be {character_name}")
+        btn_start_game = tk.Button(interface, text="Otwórz drugie okno", font=("Arial", 14), command=otworz_drugie_okno(character_name))
+        btn_start_game .grid(row=4, column=2, padx=10, pady=10)
 
 
 
@@ -16,7 +19,7 @@ interface = tk.Tk()
 interface.title('Interface')
 interface.geometry('800x500')
 
-label = tk.Label(interface, text="Weclome to the FirstFight", font=("Arial", 16))
+label = tk.Label(interface, text="Welcome to the FirstFight", font=("Arial", 16))
 label.grid(row=0, column=2, padx=10, pady=10)
 
 label = tk.Label(interface, text="Chose name of your character!", font=("Arial", 16))
@@ -25,8 +28,8 @@ label.grid(row=1, column=1, padx=10, pady=10)
 entry = tk.Entry(interface, font =("Arial", 14))
 entry.grid(row=1, column=2, padx=10, pady=10)
 
-button = tk.Button(interface, text="ready", font=("Arial", 14), command=shown_chosen_name)
-button.grid(row=1, column=3, padx=10, pady=10)
+btn_save_character_name = tk.Button(interface, text="ready", font=("Arial", 14), command=shown_chosen_name)
+btn_save_character_name.grid(row=1, column=3, padx=10, pady=10)
 
 input_character_name = tk.Label(interface, text="", font=("Arial", 14))
 input_character_name.grid(row=2, column=1, padx=10, pady=10)
