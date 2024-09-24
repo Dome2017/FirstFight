7f53704abc3ee6b44432ee2056cddf3e3a96c22e
Dome2017 <dominik.monkos@gmail.com>
2024-09-24 17:44:34 +0100

    wprowadzono zmiany dzięki którym na drugim ekranie są wyświetlnae także statystyki, obraz i nazwa gracza nr 2

--- src/battle_interface.py
+++ src/battle_interface.py
@@ -3,13 +3,13 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 from src.models.character_model import Character
+from src.models.character_model import Playercharacter
 
-
-def otworz_drugie_okno(postac1: Character):
+def otworz_drugie_okno(postac1: Character, postac2: Playercharacter):
     ekran2 = tk.Toplevel()
     ekran2.title("Drugie okno")
     ekran2.geometry("800x800")
-
+    # funkcja do obrazu nr 1
     def pokaz_obraz(url):
         response = requests.get(url)
 
@@ -18,28 +18,52 @@ def otworz_drugie_okno(postac1: Character):
             img = img.resize((300, 350))
             img_tk = ImageTk.PhotoImage(img)
             img_label = tk.Label(ekran2)
-            img_label.pack(pady=10)
+            img_label.grid(row=0, column=1, padx=10, pady=10)
+            img_label.configure(image=img_tk)
+            img_label.image = img_tk
+    # funkcja do obrazu nr 2
+    def pokaz_obraz2(url):
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            img = Image.open(BytesIO(response.content))
+            img = img.resize((300, 350))
+            img_tk = ImageTk.PhotoImage(img)
+            img_label = tk.Label(ekran2)
+            img_label.grid(row=0, column=4, padx=10, pady=10)
             img_label.configure(image=img_tk)
             img_label.image = img_tk
 
 
 ############################# Rysowanie Zawartosci ekranu
-    label_player_name = tk.Label(ekran2, text=f"Your name is {postac1.name}", font=("Arial", 14))
-    label_player_name.pack(pady=20)
+    label_player_name = tk.Label(ekran2, text=f"Player {postac1.name}", font=("Arial", 14))
+    label_player_name.grid(row=1, column=1, padx=10, pady=10)
+    label_player_name = tk.Label(ekran2, text=f"Player {postac2.name}", font=("Arial", 14))
+    label_player_name.grid(row=1, column=4, padx=10, pady=10)
 
-    pokaz_obraz('https://as1.ftcdn.net/v2/jpg/05/78/94/44/1000_F_578944489_ZyfZPsK703HOOx8E08NnacYXyMoG7qJY.jpg')  # Wywołujemy funkcję, aby obraz wyświetlił się od razu po otwarciu okna
+    # Wywołujemy funkcję, aby obraz wyświetlił się od razu po otwarciu okna
+    pokaz_obraz(postac1.picURL)
+    pokaz_obraz2(postac2.picURL)
 
-    label_stats = tk.Label(ekran2, text="Statistics:", font=("Arial", 14))
-    label_stats.pack(pady=20)
+    label_stats = tk.Label(ekran2, text="Player 1 Statistics:", font=("Arial", 14))
+    label_stats.grid(row=2, column=1, padx=10, pady=10)
+    label_stats = tk.Label(ekran2, text="Player 2 Statistics:", font=("Arial", 14))
+    label_stats.grid(row=2, column=4, padx=10, pady=10)
 
     label_health = tk.Label(ekran2, text=f"Your health: {postac1.hp_player}", font=("Arial", 14))
-    label_health.pack(pady=5)
+    label_health.grid(row=3, column=1, padx=10, pady=10)
+    label_health = tk.Label(ekran2, text=f"Your health: {postac2.hp_player}", font=("Arial", 14))
+    label_health.grid(row=3, column=4, padx=10, pady=10)
 
     label_attack = tk.Label(ekran2, text=f"Attack power: {postac1.attack_player}", font=("Arial", 14))
-    label_attack.pack(pady=5)
+    label_attack.grid(row=4, column=1, padx=10, pady=10)
+    label_attack = tk.Label(ekran2, text=f"Attack power: {postac2.attack_player}", font=("Arial", 14))
+    label_attack.grid(row=4, column=4, padx=10, pady=10)
 
     label_defense = tk.Label(ekran2, text=f"Defense: {postac1.defense_player}", font=("Arial", 14))
-    label_defense.pack(pady=5)
+    label_defense.grid(row=5, column=1, padx=10, pady=10)
+    label_defense = tk.Label(ekran2, text=f"Defense: {postac2.defense_player}", font=("Arial", 14))
+    label_defense.grid(row=5, column=4, padx=10, pady=10)
 
     button_close = tk.Button(ekran2, text="Zamknij", command=ekran2.destroy)
-    button_close.pack(pady=10)
+    button_close.grid(row=6, column=2, padx=10, pady=10)
--- src/index.py
+++ src/index.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import random
 from battle_interface import otworz_drugie_okno
 from src.models.character_model import Character
-
+from src.models.character_model import Playercharacter
 interface = tk.Tk()
 interface.title('Interface')
 interface.geometry('600x800')
@@ -22,14 +22,14 @@ def set_value(napis_przed_input, row, domyslna_wartosc):
 
 def zapisz_wartosci():
     # 1 postać
-    player_character1 = Character
+    player_character1 = Playercharacter
     player_character1.name = fields['name'].get()  # Pobieramy wartość z pola tekstowego
     player_character1.picURL = fields['picURL'].get()
     player_character1.hp_player = fields['hp'].get()
     player_character1.attack_player = fields['attack'].get()
     player_character1.defense_player = fields['defense'].get()
+
     # 2 postać
-    otworz_drugie_okno(player_character1)
     player_character2 = Character
     player_character2.name = fields['name2'].get()  # Pobieramy wartość z pola tekstowego
     player_character2.picURL = fields['picURL2'].get()
@@ -37,8 +37,8 @@ def zapisz_wartosci():
     player_character2.attack_player = fields['attack2'].get()
     player_character2.defense_player = fields['defense2'].get()
 
-
-
+    # przenosi dane z wyboru dla obu postaci na drugie okno
+    otworz_drugie_okno(player_character1, player_character2)
 
 ######################## Definicja ekranu
 
@@ -47,7 +47,7 @@ label.grid(row=0, column=2, padx=10, pady=10)
 
 # pola tekstowe zapisu dla postaci nr 1
 fields['name'] = set_value('Podaj imie postaci: ', 2,'')
-fields['picURL'] = set_value('Podaj URL zdjęcia postaci: ', 3, '')
+fields['picURL'] = set_value('Podaj URL zdjęcia postaci: ', 3, 'https://as1.ftcdn.net/v2/jpg/05/78/94/44/1000_F_578944489_ZyfZPsK703HOOx8E08NnacYXyMoG7qJY.jpg')
 fields['hp'] = set_value('Podaj życie postaci: ', 4, 100)
 fields['attack'] = set_value('Podaj moc ataku postaci: ', 5, 50)
 fields['defense'] = set_value("Podaj punkty obrony postaci: ", 6, 20)
@@ -60,15 +60,13 @@ postac2_random_attack = random.randint(5,50)
 postac2_random_defense = random.randint(5,50)
 # pola tekstowe zapisu dla postaci nr 2
 fields['name2'] = set_value('Podaj imie postaci: ', 8, 'Postać 2')
-fields['picURL2'] = set_value('Podaj URL zdjęcia postaci: ', 9, 'URL do obrazu dla postaci 2')
+fields['picURL2'] = set_value('Podaj URL zdjęcia postaci: ', 9, 'https://t3.ftcdn.net/jpg/05/73/92/02/360_F_573920251_veCkGPsuqXwZ8pXOl1WulBjho09F3g7u.jpg')
 fields['hp2'] = set_value('Podaj życie postaci: ', 10, postac2_random_hp)
 fields['attack2'] = set_value('Podaj moc ataku postaci: ', 11, postac2_random_attack)
 fields['defense2'] = set_value("Podaj punkty obrony postaci: ", 12, postac2_random_defense)
 # przycisk otwarcia drugiego okna
 button = tk.Button(interface, text="zaprezentuj postacie", command=zapisz_wartosci)
 button.grid(row=14, column=2, pady=20)
-
-
 
 
 interface.mainloop()
